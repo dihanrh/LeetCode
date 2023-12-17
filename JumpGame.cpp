b2e9c0cf57be522b9a79441a1550e6d666f98f9c
dihanrh <code.dihan@gmail.com>
2023-12-18 00:48:57 +0600

    Jump Game

---
+++ JumpGame.cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/jump-game/
+//Jump Game
+
+// TC : O(n)
+// SC : O(1)
+
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        int goal = n-1;
+        for(int i = n - 2; i >= 0; i --)
+        {
+            if(i+nums[i] >= goal)
+            {
+                goal = i;
+            }
+        }
+        return (goal == 0 ? true : false);
+    }
+};
